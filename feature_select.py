dd97dad97636334fe35d035db0c88c3a04d58307
brentmfoster <bf400@exeter.ac.uk>
2025-12-17 09:30:03 +0000

    Update feature_select.py

--- feature_select.py
+++ feature_select.py
@@ -1,7 +1,8 @@
 import feature_select_lib as fsl
 import pandas as pd
 
-# Load the counts dataframe 
+# Load the counts dataframe
+print(f"Loading counts dataframe")
 counts = pd.read_csv(
     "scMultiSim_counts_SIMPLE.csv",
     index_col=0,
@@ -9,16 +10,20 @@ counts = pd.read_csv(
 )
 
 # Preprocess the counts dataframe
+print(f"Preprocessing counts dataframe")
 transposed_counts = fsl.preprocess_dataframe(counts)
 # Check that gene names are columns (gene1-geneX) & the index is cell types (A-I)
 transposed_counts
 
 # Define parameters & scale the data
+print(f"Defining model parameters & scaling the data")
 parameters = fsl.model_parameters(transposed_counts)
 
 # Loop over feature selection-classifier pipeline, fit the model,
 # evaluate with accuracy scores, classificatin reports, confusion matrix plots
+print(f"Running model loop")
 fsl.run_model(parameters)
 
 # Plot a UMAP that shows trajectory
+print(f"Plotting UMAP")
 fsl.plot_umap(transposed_counts, parameters)
