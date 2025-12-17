dd72d6af2166962efb2ee35fbfd130c441d84e8d
brentmfoster <bf400@exeter.ac.uk>
2025-12-17 09:53:46 +0000

    Update feature_select.py

--- feature_select.py
+++ feature_select.py
@@ -2,7 +2,7 @@ import feature_select_lib as fsl
 import pandas as pd
 
 # Load the counts dataframe
-print(f"Loading counts dataframe")
+print(f"\nLoading counts dataframe")
 counts = pd.read_csv(
     "scMultiSim_counts_SIMPLE.csv",
     index_col=0,
@@ -10,22 +10,22 @@ counts = pd.read_csv(
 )
 
 # Preprocess the counts dataframe
-print(f"Preprocessing counts dataframe")
+print(f"\nPreprocessing counts dataframe")
 transposed_counts = fsl.preprocess_dataframe(counts)
 # Check that gene names are columns (gene1-geneX) & the index is cell types (A-I)
 transposed_counts
 
 # Define parameters & scale the data
-print(f"Defining model parameters & scaling data")
+print(f"\nDefining model parameters & scaling data")
 parameters = fsl.model_parameters(transposed_counts)
 
 # Loop over feature selection-classifier pipeline, fit the model,
 # evaluate with accuracy scores, classificatin reports, confusion matrix plots
-print(f"Comparing model accuracy scores, classification reports & confusion matrices")
+print(f"\nComparing model accuracy scores, classification reports & confusion matrices")
 fsl.run_model(parameters)
 
 # Plot a UMAP that shows cell trajectory
-print(f"Plotting cell trajectory UMAP")
+print(f"\nPlotting cell trajectory UMAP")
 fsl.plot_umap(transposed_counts, parameters)
 
-print(f"Feature select is finished running!")
+print(f"\nFeature select is finished running!\n")
